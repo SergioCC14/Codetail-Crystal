d238c157f8b0a0130191f17149c40472c9e4c202
Sergio Canis <sergio.canis@outlook.com>
2017-06-19 13:12:27 +0200

    add comment

--- codetails_crystal_app/src/6_kemal.cr
+++ codetails_crystal_app/src/6_kemal.cr
@@ -11,4 +11,4 @@ Kemal.run
 # ruby test_kemal.rb
 # crystal src/6_kemal.cr
 # echo "GET http://0.0.0.0:3000" | vegeta attack -rate 600 -duration 10s | vegeta report
-# echo "GET http://0.0.0.0:4567" | vegeta attack -rate 10 -duration 20s | vegeta report
+# echo "GET http://0.0.0.0:4567" | vegeta attack -rate 600 -duration 10s | vegeta report
